02e564a6c51d2ae224fc9e25088f146a485e64ea
phie <63649591+phie73@users.noreply.github.com>
2024-06-02 14:41:43 +0200

    Using wcif to recive comp data (#9)
    * added gitignore
    
    * no need to install everything when you just can do something like pip install -r requirements.txt
    
    * somehow intermediate state, getting data from wcif (or at least something like that)
    
    * added latex stuff to .gitignore
    
    * restructured latex stuff
      * idea is to have one singel file (lables-template.tex) that never is toched
      * storing all the comp related stuff in name.tex and content.tex
      * requesting everything, every choise I made in my life as always when really doing something with latex
    
    * tex: restructured, moved everythin into subfolder
    
    * tex: small adjustments, table only one argument, handeling long comp names
    
    * python: somehow generating name tag info
    
    * python: names replacement latex stupid stuff, country iso to name
    :wq
    
    * somehow sloppy adapted
    
    * tex: woher soll ich wissen was ich hier tue, latex ist nur ein großen macro geschiebe und gehoffe (ernsthaft, wer versteht das schon)
    
    * newcomer in green, optionally print orga and delegate roles on nametag
    
    * resizing long names to fit on one line
    
    * updated readme
    
    * ordering of events and cards
    
    * removed commented out code
    
    * sorting
    
    * cyrill letters support
    
    * 90mm tags
    
    * general cleanup, sorting of events
    
    * cyrill letters, error in request handeling, cleanup
    
    * Update README.md
    
    * cleanup #2
    
    * .
    
    * welp cleanup #73
    
    * Update README.md
    
    * finally added generated stuff to gitignore
    
    * cleanup latex stuff
    
    * support for venues with multiple rooms/stages
    
    * Update README.md (added some more todos)
    
    * Update requirements.txt (removed uneeded stuff)
    
    * Update README.md
    
    added some more todos

--- main.py
+++ main.py
@@ -1,125 +1,104 @@
 import argparse
-import numpy as np
-import pandas as pd
+import requests
+import json
+import re 
+import pycountry
+from collections import OrderedDict
 
 # configuration
 parser = argparse.ArgumentParser(description="Main script")
-parser.add_argument('-c',"--competitors",
-                    help="Path to competitors.csv (without file extension)")
-parser.add_argument('-id',"--comp_id",
-                    help="Short name of the competition by which one can access the website")
+parser.add_argument('-c',"--comp_id",
+                    help="competition id")
 parser.add_argument('-o',"--order_by",
                     help="Order of nametags for printing, options: id, name, default: id",
                     default='id')
-#parser.add_argument('-l',"--layout",
-#                    help="Number of nametags to place on a page,\
-#                    given by rows x columns x (h/p)\
-#                    where h is for horizontal, p for portrait mode", default="5 x 2 x p")
-# currently, layout is just handled in preamble of the document itself, independent of content
+parser.add_argument('-od', "--print_orga_delegate",
+                    help="print orga and delegate role on nametag, options y, n, default: n",
+                    default='n')
 args = parser.parse_args()
 
 # read in user args
-competitors_path = args.competitors
 comp_id = args.comp_id
 order_by = args.order_by
-#layout = args.layout
-# currently, layout is just handled in preamble of the document itself, independent of content
-
-# hard-coded stuff
-WCA_database_path = '../wca-competition-orga/WCA_export.tsv/'
-
-# ToDo:
-# - run validation before trying to read info à la `import validation from external; validation()`
-#   - competitors.csv exists
-#   - comp_id resolves to a real website
-#   - layout makes sense and fits on a sheet
+print_orga_delegate = args.print_orga_delegate
 
 # get competitor information for specific competition
-competitors = pd.read_csv(f'{competitors_path}.csv')
-competitors_also_deleted = pd.read_csv(f'{competitors_path}-all.csv')
+res = requests.get("https://worldcubeassociation.org/api/v0/competitions/" + comp_id + "/wcif/public")
 
-# do not want zero-counting, we are humans and start with * 1 * :-)
-competitor_ids = np.array(competitors_also_deleted.loc[competitors_also_deleted.Name.isin(competitors.Name)].index + 1)
-competitors['competitor_ids'] = competitor_ids
+# if wca page has issues totaly fine to just throw here, not intended to continue in this case
+res_json = res.json()
+if "error" in res_json:
+    print(res_json["error"])
+    quit()
+
+# multiple rooms
+activities = []
+for x in range(len(res_json['schedule']['venues'][0]['rooms'])):
+    activities += res_json['schedule']['venues'][0]['rooms'][x]['activities']
+
+activityIdMap = dict()
+for activity in activities:
+    activityIdMap[activity['id']] = activity['activityCode']
+    for child in activity['childActivities']:
+        activityIdMap[child['id']] = child['activityCode']
+persons = res_json['persons']
 
 if order_by == 'name':
-    competitors.sort_values(by=['Name'], inplace=True)
-    competitors.reset_index(drop=False, inplace=True)
+    persons = sorted(persons, key=lambda persons: persons['name'])
 
-names = competitors['Name']
-countries = competitors['Country']
-# replace NaN Ids with Str 'Newcomer' for proper printout
-competitors['WCA ID'].replace(np.nan, 'Newcomer', inplace=True) # for all views of the column
-wca_ids = competitors['WCA ID']
-# get competition information (associated staff like organizers and delegates)
-competitions = pd.read_csv(f'{WCA_database_path}WCA_export_Competitions.tsv',sep='\t')
-this_comp = competitions[competitions['id'] == comp_id]
+# event order
+EVENTORDER = ['333', '222', '444', '555', '666', '777', '333bf', '333fm', '333oh', 'clock', 'minx', 'pyram', 'skewb', 'sq1', '444bf', '555bf', '333mbf']
 
-delegates = this_comp['wcaDelegate']
-orga = this_comp['organiser']
-comp_name = this_comp['name'].values[0]
+tex_builder = ''
+for person in persons:
+    # structure {Name}{WCAID}{country}{reg id}{assignments[event & comp & scr & judge & run\\]}
+    if (bool((re.compile(r'[\u0400-\u04FF]+')).search(person["name"])) == True): #cyrill letters
+        tmpName = '\\foreignlanguage{ukrainian}{' + person["name"] + "}"
+    elif (bool((re.compile(r'[\u4e00-\u9fff\u3000-\u303f\uff00-\uffef]')).search(person["name"])) == True): #"chinese"
+        tmpName = '\\begin{CJK*}{UTF8}{gbsn}' + person["name"] + '\\end{CJK*}'
+    else:
+        tmpName = person["name"]
+    tex_builder += '\card' + '{' + tmpName + '}'
+    tex_builder += '{' + (person["wcaId"], '\\textcolor{ForestGreen}{Newcomer}')[person["wcaId"] == None] + '}'# either wcaId or 'Newcomer'
+    tex_builder += '{' + pycountry.countries.get(alpha_2=person["countryIso2"]).name + '}'
+    tex_builder += '{' + str(person["registrantId"]) 
+    tex_builder += ('' , ('', (' \\textcolor{Red}{' + " ".join(str(x) for x in person["roles"]) + '}'))[person["roles"] != []])[print_orga_delegate == 'y'] + '}'
 
-# go from a series (pandas) to a list, then split until only names remain
-delegates_ = [d for d in delegates]
-delegates_ = delegates_[0].split('] [')
-delegates_ = [d.split('}{')[0].split('{')[1] for d in delegates_]
-orga_ = [o for o in orga]
-orga_ = orga_[0].split('] [')
-orga_ = [o.split('}{')[0].split('{')[1] for o in orga_]
-                          
-# write LaTeX snippet that can be used for labels
-for i in range(len(competitor_ids)):
-    tex_builder = '\\addresslabel{\centering{\selectlanguage{english} ' + comp_name + '\\vspace{1.1em} \Huge \\\\ '
-    
-    name_string = f'{names[i]}'
-    multi_row_name = False
-    if '(' in name_string and ')' in name_string:
-        multi_row_name = True
-        # this is a name with additional local string with other characters
-        # would require a linebreak most likely, so the string is split into two
-        # and later the second part is written with a smaller fontsize
-        name_string_a, name_string_b = name_string.split('(')
-        name_string_a = name_string_a[:-1]
-        name_string_b = name_string_b[:-1]
-        tex_builder += '\\textbf{\\centering \\begin{minipage}{82mm}\\centering '+ name_string_a + '\\end{minipage}\\vspace{1.5mm}}'
-        tex_builder += '}'
-        tex_builder += '\\\\'
-        if countries[i] == 'Russia' or countries[i] == 'Ukraine':
-            tex_builder += ' \\selectlanguage{russian} '
-        if countries[i] == 'China':
-            tex_builder += ' \\begin{CJK*}{UTF8}{gbsn} '
-            tex_builder += '(' + name_string_b + ')'
-            tex_builder += ' \\end{CJK*}'
+    assignments = dict()
+    for assignment in person['assignments']:
+        assignment['activityId'] = activityIdMap[assignment['activityId']]
+        
+        if (assignment['activityId'].startswith("333fm")):
+            pass
         else:
-            tex_builder += '(' + name_string_b + ')'
-    else:
-        tex_builder += '\\textbf{\\centering \\begin{minipage}{82mm}\\centering '+ name_string + '\\end{minipage}}}'
-    tex_builder += '\\\\ '
-    
-    
-    country_string = f'{countries[i]}'
-    wca_id_string = f'{wca_ids[i]}'
-    if wca_id_string == 'Newcomer':
-        wca_id_string = '\\textcolor{ForestGreen}{' + wca_id_string + '}'
-    competitor_id_string = competitors['competitor_ids'][i]
-    
-    is_del = True if names[i] in delegates_ else False
-    is_orga = True if names[i] in orga_ else False
-    optional_role_string = ''
-    if is_del and is_orga:
-        optional_role_string = 'Delegate, Organizer'
-    else:
-        if is_del:
-            optional_role_string = 'Delegate'
-        elif is_orga:
-            optional_role_string = 'Organizer'
-    
+            # acitvityId is build like event-round-group
+            # build dict (or whatever; it is python there are no datatypes) 'event' : '[comp,scr,judge,run]' (todo if some assignment type isn't used don't include it)
+            activity = assignment['activityId'].split('-') 
+            if not(activity[0] in assignments):
+                assignments[activity[0]] = [' ',' ',' ',' ']
+            if assignment['assignmentCode'] == 'competitor':
+                assignments[activity[0]][0] += ((activity[2])[1:], (',' + (activity[2])[1:]))[assignments[activity[0]][0] != ' '] 
+            elif assignment['assignmentCode'] == 'staff-scrambler':
+                assignments[activity[0]][1] += ((activity[2])[1:], (',' + (activity[2])[1:]))[assignments[activity[0]][1] != ' ']
+            elif assignment['assignmentCode'] == 'staff-judge':
+                assignments[activity[0]][2] += ((activity[2])[1:], (',' + (activity[2])[1:]))[assignments[activity[0]][2] != ' ']   
+            elif assignment['assignmentCode'] == 'staff-run':
+                assignments[activity[0]][3] += ((activity[2])[1:], (',' + (activity[2])[1:]))[assignments[activity[0]][3] != ' ']
 
-    if optional_role_string != '':
-        tex_builder += '\\selectlanguage{english} \\ \\\\ ' + '\\textcolor{Red}{' + optional_role_string + '} \\\\ '
-    
-    tex_builder += '\\selectlanguage{english} \\begin{tabular}{lcr}	\\qquad &  \\qquad & \\qquad  \\\\' + wca_id_string + '&' + country_string + '& ID: ' + f'{competitor_id_string}' + '\\\\ \\end{tabular}'
-    
+    # sorting
+    assignmentsSorted = OrderedDict((k, assignments[k]) for k in EVENTORDER if k in assignments)
+    tex_builder += '{'
+    for k in assignmentsSorted:
+        tex_builder += k + '&' + str(assignmentsSorted[k][0]) + '&' + str(assignmentsSorted[k][1]) + '&' + str(assignmentsSorted[k][2]) + '&' + str(assignmentsSorted[k][3]) + '\\\\'
     tex_builder += '}'
-    
-    print(tex_builder)
+
+# writing to tex/content.tex
+# overwriting everything, makes no sense if there is already something in
+f = open("tex/content.tex", "w")
+f.write(tex_builder)
+f.close()
+
+#writing comp name
+f = open("tex/name.tex", 'w')
+f.write(res_json['name'])
+f.close()
